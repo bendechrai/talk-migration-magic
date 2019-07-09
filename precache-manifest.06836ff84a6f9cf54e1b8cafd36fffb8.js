5f6156faa87315a3c4dc71cbc2707d47cd21dbce
Ben Dechrai <ben@dechrai.com>
2019-07-09 14:54:34 +0800

    Updates

---
+++ precache-manifest.06836ff84a6f9cf54e1b8cafd36fffb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7181ad6c29ccde67328fbe3ee17066d",
+    "url": "/talk-migration-magic/index.html"
+  },
+  {
+    "revision": "9f0fa95d8134e4206953",
+    "url": "/talk-migration-magic/static/css/main.9d5253b6.chunk.css"
+  },
+  {
+    "revision": "2b5052cc54db8d58d5ce",
+    "url": "/talk-migration-magic/static/js/2.bbf259e6.chunk.js"
+  },
+  {
+    "revision": "9f0fa95d8134e4206953",
+    "url": "/talk-migration-magic/static/js/main.2e721554.chunk.js"
+  },
+  {
+    "revision": "6d67fad1785cebe95e39",
+    "url": "/talk-migration-magic/static/js/runtime~main.2bf0cb5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/talk-migration-magic/static/media/logo.5d5d9eef.svg"
+  }
+]);
